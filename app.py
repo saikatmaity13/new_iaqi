bedf7c3e4ed261e0b45bb5c377b5dd538948c5a7
saikatmaity13 <146626683+saikatmaity13@users.noreply.github.com>
2025-09-13 09:44:02 +0530

    Configured app to run on Render's host and port

--- app.py
+++ app.py
@@ -152,5 +152,10 @@ def forecast_metric(metric):
         return jsonify({"error": str(e)}), 500
 
 
+import os
+
+# ... (rest of your code)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
